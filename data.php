5b2b87fb54da2a1d8ccabb407bc73c456cb2cb66
ealborn <emma.alborn@gmail.com>
2017-10-29 17:43:23 +0100

    variables and author names

--- data.php
+++ data.php
@@ -1,55 +1,98 @@
 <?php
 declare(strict_types=1);
-// This is the file where you can keep all your data such as articles and
-// authors.
 
-//content variables
+//content variables for shorter array data below
 $catCeption="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
-$funkyCat = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+$meow = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
-$evilCat = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+$caturday = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
-$nisseTheCat ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+$purrFect ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
-$meowy = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
+$catInaHat = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
 
-$purrFect = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
+$scratching = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
 
+$cchaseMice = ""
+
+$lolcat = ""
+
+$grumpy = ""
+
+$worldDom = ""
+
+// array of content
 $newsitem = [
   [
     'title' => 'Catception',
     'content' => $catCeption,
     'author' => ['id' => '01', 'name' => 'The Sphynx'],
     'date' => '2014-11-13',
-    'likecount' => 16, 'http://inceptionmovie.warnerbros.com/',
+    'likecount' => '16',
   ],
   [
-    'title' => 'Funky Cat',
-    'content' => $meowy,
+    'title' => 'Meow',
+    'content' => $meow,
+    'author' => ['id' => '01', 'name' => 'The Sphynx'],
+    'date' => '2014-11-13',
+    'likecount' => '16',
+  ],
+  [
+    'title' => 'Caturday',
+    'content' => $caturday,
     'author' => ['id' => '02', 'name' => 'Kitty'],
     'date' => '2017-10-25',
-    'likecount' => 11,
+    'likecount' => '11',
   ],
   [
-    'title' => 'Crazy Cat',
+    'title' => 'Purr',
     'content' => $purrFect,
-    'author' => ['id' => '03', 'name' => 'KityCat'],
+    'author' => ['id' => '03', 'name' => 'Kitty'],
     'date' => '2016-12-20',
-    'likecount' => 25,
+    'likecount' => '25',
   ],
   [
-    'title' => 'Fizzy Cat',
-    'content' => $catCeption,
-    'author' => ['id' => '04', 'name' => 'Kitty'],
+    'title' => 'Hat of the day',
+    'content' => $catInaHat,
+    'author' => ['id' => '04', 'name' => 'Curly Cat'],
     'date' => '2017-05-21',
     'likecount' => '15',
   ],
   [
-    'title' => 'Curly Cat',
-    'content' => $catCeption,
+    'title' => 'Scratching myself',
+    'content' => $scratcing,
     'author' => ['id' => '05', 'name' => 'Curly Cat'],
     'date' => '2016-03-20',
-    'likecount' => 12,
+    'likecount' => '12',
+  ],
+
+  [
+    'title' => 'Chasing mice',
+    'content' => $chaseMice,
+    'author' => ['id' => '05', 'name' => 'Tiger'],
+    'date' => '2016-03-20',
+    'likecount' => '12',
+  ],
+  [
+    'title' => 'Lolcat',
+    'content' => $lolcat,
+    'author' => ['id' => '05', 'name' => 'Tiger'],
+    'date' => '2016-03-20',
+    'likecount' => '12',
+  ],
+  [
+    'title' => 'Grumpy',
+    'content' => $grumpy,
+    'author' => ['id' => '05', 'name' => 'Grumpy Cat'],
+    'date' => '2016-03-20',
+    'likecount' => '12',
+  ],
+  [
+    'title' => 'World domination',
+    'content' => $worldDom,
+    'author' => ['id' => '05', 'name' => 'Grumpy Cat'],
+    'date' => '2016-03-20',
+    'likecount' => '12',
   ],
 ];
